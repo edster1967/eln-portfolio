ff3c8583ee0617c626b16a68747d947a03532e9f
unknown <edwardnatividad@gmail.com>
2019-06-18 20:19:26 -0400

    updated page layout and navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  navbarOpen = false;
+  navLinkItems = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.navLinkItems = [
+      {
+        routerLinkItem: '/home',
+        routerLinkName: 'Home'
+
+      },
+      {
+        routerLinkItem: '/photos',
+        routerLinkName: 'Photos'
+
+      },
+      {
+        routerLinkItem: '/projects',
+        routerLinkName: 'Projects'
+
+      }
+    ];
+  }
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+
+  toggleNavBarItem() {
+    this.navbarOpen = false;
   }
 
 }
--- src/app/photos/photos.component.ts
+++ src/app/photos/photos.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 
 
 @Component({
@@ -9,54 +8,11 @@ import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gal
 })
 export class PhotosComponent implements OnInit {
 
-  galleryOptions: NgxGalleryOptions[];
-  galleryImages: NgxGalleryImage[];
 
   constructor() { }
 
   ngOnInit(): void {
-    this.galleryOptions = [
-      {
-        width: '100%',
-        height: '400px',
-      // breakpoint: 500,
-        thumbnailsColumns: 3,
-        imageSize: 'contain',
-        imageAnimation: NgxGalleryAnimation.Slide
-    }
-    ];
-    this.galleryImages = [
-      {
-        small: 'assets/img/gc_eln.jpg',
-        medium: 'assets/img/gc_eln.jpg',
-        big: 'assets/img/gc_eln.jpg'
-      },
-      {
-        small: 'assets/img/redrock_eln.jpg',
-        medium: 'assets/img/redrock_eln.jpg',
-        big: 'assets/img/redrock_eln.jpg'
-      },
-      {
-        small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/2-small.jpeg',
-        medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/2-medium.jpeg',
-        big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/2-big.jpeg'
-      },
-      {
-        small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/3-small.jpeg',
-        medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/3-medium.jpeg',
-        big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/3-big.jpeg'
-      },
-      {
-        small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/4-small.jpeg',
-        medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/4-medium.jpeg',
-        big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/4-big.jpeg'
-      },
-      {
-        small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/5-small.jpeg',
-        medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/5-medium.jpeg',
-        big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/5-big.jpeg'
-      }
-    ];
+
   }
 
 }
